32cf0e6c6ca6b484c6d3f8935e76242a2d13d458
Mynul Alam <mynulalam27@gmail.com>
2025-07-18 16:21:41 +0600

    ui design

--- lib/screen/card_stack_ui_design.dart
+++ lib/screen/card_stack_ui_design.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/consts/images.dart';
 
 class TravelUI extends StatelessWidget {
   const TravelUI({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(child: Column(children: [Image.asset(image1)])),
+          SizedBox(height: 10),
+          Text(
+            "Tap Destination",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          Container(),
+
+          SizedBox(height: 10),
+          Text(
+            "Trending Packages",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(),
+          SizedBox(height: 10),
+        ],
+      ),
+    );
   }
 }
