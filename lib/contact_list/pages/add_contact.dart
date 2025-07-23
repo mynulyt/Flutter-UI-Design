4e7b510ef710401d05d4a4a65c4eb37ca0b9a1f1
Mynul Alam <mynulalam27@gmail.com>
2025-07-23 23:44:08 +0600

    save contact

--- lib/contact_list/pages/add_contact.dart
+++ lib/contact_list/pages/add_contact.dart
@@ -8,19 +8,33 @@ class AddContact extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Row(
-            children: [
-              Icon(Icons.cancel),
-              Text(
-                "Save Number",
-                style: TextStyle(
-                  color: Colors.black45,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
+          SizedBox(
+            height: 60,
+            width: 450,
+            child: Row(
+              children: [
+                Icon(Icons.cancel),
+                Text(
+                  "Save Number",
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
+                Icon(Icons.ramp_right),
+              ],
+            ),
+          ),
+          CircleAvatar(),
+          TextField(
+            decoration: InputDecoration(
+              label: Text("Name"),
+              prefixIcon: Icon(Icons.arrow_downward),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              Icon(Icons.ramp_right),
-            ],
+            ),
           ),
         ],
       ),
