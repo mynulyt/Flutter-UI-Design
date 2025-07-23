b1d8bb9e2f2fddecbc6ecd4425e668873aca8dfc
Mynul Alam <mynulalam27@gmail.com>
2025-07-23 22:48:55 +0600

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/contact_list/pages/home_page.dart';
 import 'package:flutter_ui/screen/card_stack_ui_design.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(body: TravelUI()),
+      home: Scaffold(body: HomePage()),
     );
   }
 }
