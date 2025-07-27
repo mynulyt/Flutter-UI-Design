95a5e6ff84a0bea6c8202b171463d18be096ff3c
Mynul Alam <mynulalam27@gmail.com>
2025-07-27 22:55:24 +0600

    assignment

--- lib/assigment_module_10/assignment.dart
+++ lib/assigment_module_10/assignment.dart
@@ -5,6 +5,6 @@ class AssignmentModule10 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: Container(child: Column()));
   }
 }
