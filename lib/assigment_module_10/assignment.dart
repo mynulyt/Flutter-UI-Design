800da38507d7c715ea453edf600e5e919a76c7ea
Mynul Alam <mynulalam27@gmail.com>
2025-07-27 23:35:54 +0600

    assignment

--- lib/assigment_module_10/assignment.dart
+++ lib/assigment_module_10/assignment.dart
@@ -9,6 +9,7 @@ class AssignmentModule10 extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
+            SizedBox(height: 60),
             Text(
               "Hello, World!",
               style: TextStyle(
@@ -17,6 +18,7 @@ class AssignmentModule10 extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(height: 20),
             Text(
               "Welcome to Flutter!",
               style: TextStyle(
@@ -29,17 +31,28 @@ class AssignmentModule10 extends StatelessWidget {
               "https://tse1.mm.bing.net/th/id/OIP.X0KMz7V4ozhJiB1l-4V1ZwHaEK?w=1280&h=720&rs=1&pid=ImgDetMain&o=7&rm=3",
             ),
             SizedBox(
-              height: 20,
-              width: 450,
+              height: 60,
+              width: 400,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(15),
                   ),
                 ),
-                onPressed: () {},
-                child: Text("Press Me"),
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Button Presses")));
+                },
+                child: Text(
+                  "Press Me",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/assigment_module_10/assignment.dart';
 import 'package:flutter_ui/contact_list/pages/home_page.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(body: HomePage()),
+      home: Scaffold(body: AssignmentModule10()),
     );
   }
 }
